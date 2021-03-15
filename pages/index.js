cbac8d94361c25f4c11ee2c7acfbfd25dd996e26
Fernanda Leite <contato@nandaleite.com>
2021-03-15 13:47:07 -0300

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../styles/Home.module.scss'
+import SearchBox  from '../components/SearchBox';
+import Loading from '../components/Loading';
+import UserCard from '../components/UserCard';
+import { useAppContext } from '../context'
 
-export default function Home() {
+const Home = () => {
+  const { 
+    username,
+    userData,
+    loading,
+    error,
+    setUsername,   
+    setUserData,
+    setLoading,
+    setError 
+  } = useAppContext()
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Search Github User</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Busca de usuário Github
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className="grid">
+          <SearchBox />
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="grid">
+          {loading && <Loading />}
+        </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="grid">
+          {userData && <UserCard />}
+          {error ? error : ''}
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <Link href="https://github.com/nandaleite">
+          <a>Made by Fernanda Leite</a>
+        </Link>
       </footer>
     </div>
   )
 }
+
+export default Home
